f82fd825f698e21545dbfa69a1d5f0b1798d4f3d
Esther Wandera <wanderaesther809@gmail.com>
2026-01-13 01:06:14 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,25 +1,16 @@
-# server/app.py
-
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
-# create a Flask application instance 
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# Connects to the local database file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
+# Setup migration tools
 migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
 db.init_app(app)
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,21 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
+# This helps with naming constraints in the database
 metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
+# Create the extension
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
-
-
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Employee(db.Model):
+    __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    salary = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f'<Employee {self.id}, {self.name}, {self.salary}>'
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}, {self.address}>'
